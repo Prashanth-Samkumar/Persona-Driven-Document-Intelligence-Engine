a45dbeeffc0f248d0a8589dcaa2158e07fa1da2a
Prashanth Samkumar <134489728+Prashanth-Samkumar@users.noreply.github.com>
2026-05-17 00:28:39 +0530

    Add new lines for better code organization

--- backend/service/api.py
+++ backend/service/api.py
@@ -8,6 +8,8 @@ import os
 
 load_dotenv()
 
+
+
 PINECONE_API_KEY = os.getenv("PINECONE_API_KEY")
 PINECONE_ENV= os.getenv("PINECONE_ENV", "us-west1-gcp") 
 PINECONE_INDEX_NAME = os.getenv("PINECONE_INDEX_NAME")
